65c91ba5c429cc2f1c6e078979d7a13a1eb4e697
Erik Martin <erikmartinfernandez@gmail.com>
2024-11-06 08:37:49 +0000

    revision

--- src/app.js
+++ src/app.js
@@ -6,6 +6,27 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let icon = ["♦", "♥", "♠", "♣"];
+  let number = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "K",
+    "Q",
+    "J"
+  ];
+
+  let randomIcon = Math.floor(Math.random() * icon.length);
+  let randomNumber = Math.floor(Math.random() * number.length);
+
+  document.querySelector("#icon-top").innerHTML = icon[randomIcon];
+  document.querySelector("#icon-bottom").innerHTML = icon[randomIcon];
+  document.querySelector(".number").innerHTML = number[randomNumber];
 };
